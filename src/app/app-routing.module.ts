49dfd5f1ed5bdead66ab497f4b52377ad8a2b1e2
cedrickdeguzman <deguzmancedrick95@gmail.com>
2019-11-28 17:52:37 +0700

    etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+   path: 'dashboard'; component: DashboardComponent; }
+
